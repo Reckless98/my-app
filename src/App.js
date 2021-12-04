4ce22d59f5f70e99de05355422a0a46e32c21438
Reckless98 <filipwolf@gmail.com>
2021-12-04 14:52:20 +0100

    licecycle managment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Form from './components/Form'
+import LifecycleA from './components/LifecycleA'
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <LifecycleA />
+      {/* <Form /> */}
     </div>
   );
 }
